50bb8da8c3b324f0d64acc6e03cafdae95b237df
Roman Yagafarov <roma.yagafarov@gmail.com>
2020-08-11 10:24:15 +0500

    Syntax

--- FaceComparison.cs
+++ FaceComparison.cs
@@ -23,22 +23,20 @@ namespace FaceDetect
             Console.WriteLine("Start scan Database");
             db DataBase = new db();
             List<string> DBImageFileNames = DataBase.GetAllPhotos(); //База данных названий картинок
-            IList<Guid?> targetFaceIds = new List<Guid?>(); //Создание нового списка ID для людей
+            IList<Guid?> TargetFaceIds = new List<Guid?>(); //Создание нового списка ID для людей
             foreach (var ImageFileName in DBImageFileNames) //Создание ID для каждой картинки из базы
             {
                 var Faces = await DetectFaceRecognize(Client, $"{PathToBase}{ImageFileName}", RecognitionModel); //Запуск асинхр потока с поиском лиц
                 if (Faces == null) { return; } //На случай, если лица не обнаружены
-                targetFaceIds.Add(Faces[0].FaceId.Value); //Засунуть в список человека
+                TargetFaceIds.Add(Faces[0].FaceId.Value); //Засунуть в список человека
                 DataBase.UpdateGUID(Faces[0].FaceId.Value, ImageFileName); //Обновить значение ID для человека в стобце GUID БД
             }
             Console.WriteLine("Database successfull scanned");
             //Запуск асинхр потока с обнаружением лиц на ВХОДНОМ фото
             IList<DetectedFace> DetectedFaces = await DetectFaceRecognize(Client, $"{PathToBase}{InputImageFileName}", RecognitionModel);
-            // Поиск похожих лиц в списке GUID. Метод FindSimilarAsync (поиск похожих в асинхронном потоке. В метод передаем обнаруженное лицо
-            //на ВХОДНОМ фото, и сравниваем с базой всех лиц)
             if (DetectedFaces != null) //Если найдено 1 лицо (не больше и не меньше) (метод DetectFaceRecognize не вернул null) тогда работаем
             {
-                IList<SimilarFace> SimilarResults = await Client.Face.FindSimilarAsync(DetectedFaces[0].FaceId.Value, null, null, targetFaceIds);
+                IList<SimilarFace> SimilarResults = await Client.Face.FindSimilarAsync(DetectedFaces[0].FaceId.Value, null, null, TargetFaceIds); //Azure FaceApi - Поиск похожих лиц поступившего в программу фото и []БД
                 if (SimilarResults.Count != 0)
                 {
                     foreach (var SimilarResult in SimilarResults)
@@ -61,25 +59,25 @@ namespace FaceDetect
         }
 
 
-        private async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient Client, string CurrentImageFileName, string RecognitionModel) //Подсчет количества лиц на фото
+        private async Task<List<DetectedFace>> DetectFaceRecognize(IFaceClient Client, string CurrentImageFileName, string RecognitionModel) //Распознать лицо
         {
             using (FileStream stream = new FileStream(CurrentImageFileName, FileMode.Open))
             {
-                IList<DetectedFace> DetectedFaces = await Client.Face.DetectWithStreamAsync(stream, recognitionModel: RecognitionModel);
-                if (DetectedFaces == null || DetectedFaces.Count == 0) //если на фото нет лиц - ошибка
+                IList<DetectedFace> DetectedFaces = await Client.Face.DetectWithStreamAsync(stream, recognitionModel: RecognitionModel); //Вызов метода, который на Azure FaceApi ищет лицо с текущего фото
+                if (DetectedFaces == null || DetectedFaces.Count == 0) //Если на фото нет лиц - ошибка
                 {
                     Console.WriteLine($"[Error] No face detected from image `{Path.GetFileName(CurrentImageFileName)}`.");
                     return null;
                 }
-                else if (DetectedFaces.Count > 1) //если на фото больше одного лица - их слишком много
+                else if (DetectedFaces.Count > 1) //Если на фото больше одного лица - их слишком много
                 {
                     Console.WriteLine($"[Error] Too more faces from image`{Path.GetFileName(CurrentImageFileName)}`.");
                     return null;
                 }
-                else //благоприятный исход
+                else //Благоприятный исход - найдено 1 лицо
                 {
                     Console.WriteLine($"Face detected on `{Path.GetFileName(CurrentImageFileName)}`"); //Человек найден на /Изображение/
-                //    foreach (var detectedFace in detectedFaces) {Console.WriteLine($"Assigned ID = {detectedFace.FaceId.Value}"); } //вывести guid человека
+                //  foreach (var detectedFace in detectedFaces) {Console.WriteLine($"Assigned ID = {detectedFace.FaceId.Value}"); } //вывести guid человека
                     return DetectedFaces.ToList();
                 }
             }
